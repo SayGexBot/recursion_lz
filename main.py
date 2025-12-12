5a43098702ec8033e8ca080ed163cf0cab8c5eeb
SayGexBot <w1nner@tutamail.com>
2025-12-13 01:06:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import Euclid, notation, Pascal, perfection
 def main():
-    print('pascal')
     Pascal.main()
-    print("notation")
     notation.main()
-    print("perfection")
     perfection.main()
-    print("euclid")
     Euclid.main()
     
 if __name__ == "__main__":
+
     main()
